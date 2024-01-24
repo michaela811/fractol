efbcc5fb64763a6ad3255619338380fe989bfd97
michaela811 <102808406+michaela811@users.noreply.github.com>
2024-01-24 10:51:55 +0000

    another fix in events

--- events.c
+++ events.c
@@ -2,7 +2,7 @@
 
 int key_handle(int keysym, t_fractol *fractol)
 {
-    if (keysym == XK_Escape)  //x on window?
+    if (keysym == XK_Escape)
         quit_handle(fractol);
     if (keysym == XK_plus)
         fractol -> iterations += 10;
@@ -15,7 +15,7 @@ int key_handle(int keysym, t_fractol *fractol)
     else if (keysym == XK_Up)
         fractol -> shift_y -= (0.5 * fractol -> zoom);
     else if (keysym == XK_Down)
-        fractol -> shift_x += (0.5 * fractol -> zoom);
+        fractol -> shift_y += (0.5 * fractol -> zoom);
     fractol_render(fractol);
     return (0);
 }
@@ -24,7 +24,7 @@ int mouse_handle(int button, int x, int y, t_fractol *fractol)
 {
     (void)x;
     (void)y;
-        
+
     if (button == Button5)
         fractol -> zoom *= 0.95;
     else if (button == Button4)
